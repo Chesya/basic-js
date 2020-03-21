08e79786501410550ac2c6b1a1400dc4f29c6dd2
ChesyaWhite <vyacheslavbelyai@gmail.com>
2020-03-21 11:15:53 +0300

    feat: add "dream team" part

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function createDreamTeam(arr) {
+  let NameTeam = new Array();
+  if (Array.isArray(arr)) {
+      arr.forEach(el => {
+          if (typeof(el) === "string") {
+              el = el.trim().toUpperCase();
+              NameTeam.push(el[0]);
+          }
+      });
+      return NameTeam.sort().join('');
+  } else {
+    return false;
+  }
 };
